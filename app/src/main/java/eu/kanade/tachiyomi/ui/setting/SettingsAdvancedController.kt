cc5cffe5732f001b1e1be057135d4aa1b928d9e8
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2026-08-22 13:15:13 +0300

    Move novel plugins into J2K settings

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsAdvancedController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsAdvancedController.kt
@@ -15,7 +15,7 @@ import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceScreen
 import dev.ahmedmohamed.hayai.migration.LegacyMigrationRetryRequest
-import dev.ahmedmohamed.hayai.novel.plugin.ui.NovelPluginManagerActivity
+import dev.ahmedmohamed.hayai.novel.plugin.ui.NovelPluginManagerController
 import dev.ahmedmohamed.hayai.adult.eh.ui.EhSettingsActivity
 import dev.ahmedmohamed.hayai.preferences.HayaiPreferences
 import dev.ahmedmohamed.hayai.source.enhanced.batch.EnhancedBatchAddController
@@ -111,7 +111,7 @@ class SettingsAdvancedController : SettingsController() {
                 preference {
                     titleRes = R.string.hayai_novel_plugins
                     summaryRes = R.string.hayai_novel_plugins_summary
-                    onClick { startActivity(Intent(context, NovelPluginManagerActivity::class.java)) }
+                    onClick { router.pushController(NovelPluginManagerController().withFadeTransaction()) }
                 }
 
                 preference {
